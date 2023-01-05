c3817169a4a8ce004cb7b1653e8087a06ebd9b73
pogoMena <xpogoMena@gmail.com>
2023-01-05 17:38:56 -0500

    fixing for deployment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import * as ReactDOMClient from "react-dom/client";
+//import * as ReactDOMClient from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import React from "react";
 import App from "./App.js";
 
 
-const rootElement = document.getElementById("root");
+//const rootElement = document.getElementById("root");
 
-const root = ReactDOMClient.createRoot(rootElement);
-root.render(<App callback={() => console.log("renderered")} />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+//const root = ReactDOMClient.createRoot(rootElement);
+//root.render(<App callback={() => console.log("renderered")} />);
 
